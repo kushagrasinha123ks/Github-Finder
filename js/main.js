7980b409847b8b12b3dc6a4228b414b5cb642ee8
kushagra <kushagrasinha123ks@gmail.com>
2024-05-04 07:36:11 +0530

    Update main.js
    updated api-authentication details.

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
     $.ajax({
         url:'https://api.github.com/users/'+username,
         data:{
-          client_id:'b9315bcd5a07fcd759d8',
-          client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
+          client_id:'Ov23liYUBckpXkUpaC9r',
+          client_secret:'9231bbe9fe16806220d74992c5def94554194492'
         }
     }).done(function(user){
       $.ajax({
         url:'https://api.github.com/users/'+username+'/repos',
         data:{
-          client_id:'b9315bcd5a07fcd759d8',
-          client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4',
+          client_id:'Ov23liYUBckpXkUpaC9r',
+          client_secret:'9231bbe9fe16806220d74992c5def94554194492',
           sort: 'created: asc',
           per_page: 5
         }
